f2b04be4ba79735efca8c404058b4ac7180df7be
VladimirOstojic <vladaliman@gmail.com>
2020-05-05 23:25:32 +0200

    added firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,23 @@
+import { firestore } from "firebase";
+
 export const createProject = (project) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     //async call
-    dispatch({ type: "CREATE_PROJECT", project: project });
+    const firestore = getFirestore();
+    firestore
+      .collection("Projects")
+      .add({
+        ...project,
+        authorFirstName: "Vladimir",
+        authorLastName: "Ostojic",
+        authorId: 123,
+        createdAt: new Date(),
+      })
+      .then(() => {
+        dispatch({ type: "CREATE_PROJECT", project: project });
+      })
+      .catch((err) => {
+        dispatch({ type: "CREATE_PROJECT_ERR", err });
+      });
   };
 };
