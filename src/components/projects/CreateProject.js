fc24bd5395e379cd55eb7a081f214ac478e205f7
VladimirOstojic <vladaliman@gmail.com>
2020-05-10 17:43:22 +0200

    Added the signOut option and useing redirect added route guarding

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { createProject } from "../../store/actions/projectActions";
 import { connect } from "react-redux";
-
+import { Redirect } from "react-router-dom";
 class CreateProject extends Component {
   state = {
     title: "",
@@ -17,6 +17,8 @@ class CreateProject extends Component {
     });
   };
   render() {
+    const { auth } = this.props;
+    if (!auth.uid) return <Redirect to="/signin" />;
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -41,6 +43,11 @@ class CreateProject extends Component {
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -48,4 +55,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(CreateProject);
+export default connect(mapStateToProps, mapDispatchToProps)(CreateProject);
