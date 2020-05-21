6ddc1f242ce8bbd26cb927d1ed5fc55f9efde13c
VladimirOstojic <vladaliman@gmail.com>
2020-05-21 22:47:01 +0200

    conected Notifications component to firestore

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,16 +1,26 @@
 import React from "react";
+import moment from "moment";
 
-export default function Notifications() {
+export default function Notifications(props) {
+  const { notifications } = props;
   return (
     <div className="section">
       <div className="card">
         <div className="card-content">
           <span className="card-title">Notifications</span>
-          <ul>
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
+          <ul className="notifications">
+            {notifications &&
+              notifications.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <span className="blue-text">{item.user} </span>
+                    <span>{item.content}</span>
+                    <div className="grey-text note-date">
+                      {moment(item.time.toDate()).fromNow()}
+                    </div>
+                  </li>
+                );
+              })}
           </ul>
         </div>
       </div>
